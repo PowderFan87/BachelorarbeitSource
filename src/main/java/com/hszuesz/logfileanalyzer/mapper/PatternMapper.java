e079fa840f3e89df345f1b8f328eef314ab53fff
Holger Szuesz <hszuesz@live.com>
2015-08-03 00:35:45 +0200

    - Minor Changes

--- src/main/java/com/hszuesz/logfileanalyzer/mapper/PatternMapper.java
+++ src/main/java/com/hszuesz/logfileanalyzer/mapper/PatternMapper.java
@@ -1,6 +1,5 @@
 package com.hszuesz.logfileanalyzer.mapper;
 
-import com.hszuesz.logfileanalyzer.Quicktest;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,52 +12,53 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 /**
  * Extention for Hadoop class {@link Mapper} to create key by a pattern
- * 
+ *
  * Individual Mapper class to create key value pairs by searching for a key
  * via a regular expression. Configuration via properties file needed.
  *
  * @author Holger Szuesz <it12156@lehre.dhbw-stuttgart.de>
  */
 public class PatternMapper extends Mapper<Object, Text, Text, IntWritable> {
-    private final Text objTmpKey = new Text();
-    
+    public  final static    IntWritable objOne  = new IntWritable(1);
+    private final           Text objTmpKey      = new Text();
+
     /**
      * Map methode called by hadoop core.
-     * 
+     *
      * Mapping the found data to a new key value pari by extracting the key from
      * the passed value by use of a regular expression defined in the properties.
      * If the pattern isn't a match the entry is added with the key NA or skiped
      * depending on the configuration.
-     * 
+     *
      * @param objKey Object for current key
      * @param objValue Object for current value.
      * @param objContext Current context for this mapper
      * @throws IOException
-     * @throws InterruptedException 
+     * @throws InterruptedException
      */
     @Override
     public void map(Object objKey, Text objValue, Mapper.Context objContext) throws IOException, InterruptedException {
         Configuration   objConf             = objContext.getConfiguration();
         Pattern         objKeyPattern       = Pattern.compile(objConf.get("lfa.mapper.pattern.key"));
         String          strNoMatchAction    = objConf.get("lfa.mapper.nomatchaction");
-        
+
         String  strLine = objValue.toString();
-        
+
         Matcher objMatcher  = objKeyPattern.matcher(strLine);
-        
+
         if (objMatcher.find()) {
-            objTmpKey.set(objMatcher.group(1));
-        
-            objContext.write(objTmpKey, Quicktest.objOne);
+            this.objTmpKey.set(objMatcher.group(1));
+
+            objContext.write(this.objTmpKey, PatternMapper.objOne);
         } else {
             if ("SKIP".equals(strNoMatchAction)) {
                 Logger.getLogger(PatternMapper.class.getName()).log(Level.WARNING, "Can't find matching key for pattern {0}. SKIP", objConf.get("lfa.mapper.pattern.key"));
             } else {
                 Logger.getLogger(PatternMapper.class.getName()).log(Level.WARNING, "Can't find matching key. Using NA");
 
-                objTmpKey.set("NA");
-        
-                objContext.write(objTmpKey, Quicktest.objOne);
+                this.objTmpKey.set("NA");
+
+                objContext.write(this.objTmpKey, PatternMapper.objOne);
             }
         }
     }
--- src/main/java/com/hszuesz/logfileanalyzer/reducer/CountReducer.java
+++ src/main/java/com/hszuesz/logfileanalyzer/reducer/CountReducer.java
@@ -6,34 +6,34 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 /**
- * Extending {@link Reducer} fot individual use.
- * 
+ * Extending {@link Reducer} for individual use.
+ *
  * Individual extension to {@link Reducer} for creating a count of some sort.
  *
  * @author Holger Szuesz <it12156@lehre.dhbw-stuttgart.de>
  */
 public class CountReducer extends Reducer<Text, IntWritable, Text, IntWritable>  {
     private final IntWritable objTotal = new IntWritable();
-    
+
     /**
      * Sum up all values for one key and write new sum to corresponding key
-     * 
+     *
      * @param objKey Current key
      * @param arrValues Array of values for the passed key
      * @param objContext Current context
      * @throws IOException
-     * @throws InterruptedException 
+     * @throws InterruptedException
      */
     @Override
     public void reduce(Text objKey, Iterable<IntWritable> arrValues, Context objContext) throws IOException, InterruptedException {
         int lngSum = 0;
-        
+
         for(IntWritable objValue : arrValues) {
             lngSum += objValue.get();
         }
-        
+
         this.objTotal.set(lngSum);
-        
+
         objContext.write(objKey, this.objTotal);
     }
 }
